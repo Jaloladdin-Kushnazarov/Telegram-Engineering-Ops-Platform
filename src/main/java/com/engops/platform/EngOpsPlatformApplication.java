2c637567ad2d6e29359a661690f024f83bae7229
Jaloladdin-Kushnazarov <jalol2622@gmail.com>
2026-03-10 03:25:48 +0500

    test

---
+++ src/main/java/com/engops/platform/EngOpsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.engops.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngOpsPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngOpsPlatformApplication.class, args);
+    }
+
+}
